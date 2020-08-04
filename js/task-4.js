2ca8dcbeba44490c21cee0f1ed4d194df7d1404f
yuliia berehova <berehova.iuliia@gmail.com>
2020-08-05 02:18:09 +0300

    first commit

---
+++ js/task-4.js
@@ -0,0 +1,26 @@
+const countTotalSalary = function (employees) {
+  let total = 0;
+
+  for (const employee in employees) {
+    total += employees[employee];
+  }
+  return total;
+};
+
+console.log(countTotalSalary({}));
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80,
+  }),
+);
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150,
+  }),
+);
---
+++ js/task-5.js
@@ -0,0 +1,26 @@
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function (arr, prop) {
+  const propValues = [];
+
+  for (const item of arr) {
+    let propValue = item[prop];
+
+    if (propValue != undefined) {
+      propValues.push(propValue);
+    }
+  }
+
+  return propValues;
+};
+
+console.log(getAllPropValues(products, 'name'));
+
+console.log(getAllPropValues(products, 'quantity'));
+
+console.log(getAllPropValues(products, 'category'));
